5c2df4ec9a802649fb554b77657a2730cb237dc7
adamgorry <adamgorry@mac-658.lan>
2025-11-18 14:49:29 -0700

    Initial commit, project start

---
+++ CSC450CT5/src/CSC450CT5.cpp
@@ -0,0 +1,14 @@
+//============================================================================
+// Name        : CSC450CT5.cpp
+// Author      : Adam Gorry
+// Version     :
+// Copyright   : Your copyright notice
+// Description : CSC450 Critical Thinking 5
+//============================================================================
+
+#include <iostream>
+using namespace std;
+
+int main() {
+
+}
